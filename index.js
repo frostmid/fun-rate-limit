b1f633606a819ed906b59024ad4dc61563e98539
Sergey Antoninko <lyxsus@gmail.com>
2013-09-25 15:48:00 +0400

    initial

---
+++ index.js
@@ -0,0 +1,73 @@
+var _ = require ('lodash'),
+	Promises = require ('vow');
+
+function rateLimit (func, rate, async) {
+	var queue = [];
+	var timeOutRef = false;
+	var currentlyEmptyingQueue = false;
+
+	var emptyQueue = function () {
+		if (queue.length) {
+			currentlyEmptyingQueue = true;
+
+			_.delay (function() {
+				if (async) {
+					_.defer (function () {
+						var f = queue.shift ();
+						if (f) f.call ();
+					});
+				} else {
+					var f = queue.shift ();
+					if (f) f.call ();
+				}
+
+				emptyQueue ();
+			}, rate);
+		} else {
+			currentlyEmptyingQueue = false;
+		}
+	};
+
+	return function () {
+		// get arguments into an array
+		var args = _.map (arguments, function(e) {
+			return e;
+		});
+		
+		// call apply so that we can pass in arguments as parameters as opposed to an array
+		queue.push (
+			_.bind.apply (this, [func, this].concat (args))
+		);
+
+		if (!currentlyEmptyingQueue) {
+			emptyQueue ();
+		}
+	};
+};
+
+
+module.exports = {
+	sync: function (func, rate) {
+		return rateLimit (func, rate, false);
+	},
+
+	async: function (func, rate) {
+		return rateLimit (func, rate, true);
+	},
+
+	promise: function (func, rate) {
+		var limited = module.exports.sync (function (promise, args) {
+			promise.sync (
+				func.call (null, args)
+			);
+		}, rate);
+
+		return function (arguments) {
+			var promise = Promises.promise ();
+
+			limited (promise, arguments);
+
+			return promise;
+		};
+	}
+}
